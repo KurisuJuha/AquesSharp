f2ddc7ece8149bd151234d7caa36de31c114ef46
KYapp <82925909+kukiyomenaisapp@users.noreply.github.com>
2022-05-29 23:14:07 +0900

    いろんな種類でGetできるようにした

--- AquesSharp/AquesSharp.cs
+++ AquesSharp/AquesSharp.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -8,17 +9,23 @@ namespace KYapp.AquesSharp
     public static class AquesSharp
     {
         private static HttpClient client = new HttpClient();
-        public static void Get()
+        public static HttpResponseMessage Get()
         {
             Task<HttpResponseMessage> task = client.GetAsync("https://www.yukumo.net/api/v2/aqtk10/koe.mp3?type=f1e&effect=none&boyomi=false&speed=100&volume=100&pitch=100&accent=100&lmd=100&fsc=100&kanji=%E3%83%86%E3%82%B9%E3%83%88%E3%81%A7%E3%81%99%E3%82%88");
             task.Wait();
-            Task<byte[]> res = task.Result.Content.ReadAsByteArrayAsync();
-            res.Wait();
-            foreach (var item in res.Result)
-            {
-                Console.Write(item);
-            }
-            Console.WriteLine(task.Result);
+            return task.Result;
+        }
+        public static byte[] ByteGet()
+        {
+            var task = Get().Content.ReadAsByteArrayAsync();
+            task.Wait();
+            return task.Result;
+        }
+        public static Stream StreamGet()
+        {
+            var task = Get().Content.ReadAsStreamAsync();
+            task.Wait();
+            return task.Result;
         }
     }
 }
